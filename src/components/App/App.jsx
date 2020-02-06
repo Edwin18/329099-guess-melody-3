040b97cc47c4a01928458f8299885d05c91b33c8
Edwin18 <igreenbergi78@gmail.com>
2020-02-06 15:56:49 +0200

    Исправляет пробелы пропсов

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import WelcomeScreen from '../WelcomeScreen/WelcomeScreen.jsx';
-
-const App = (props) => {
+// eslint-disable-next-line react/prop-types
+const App = ({errorsCount}) => {
   return <React.Fragment>
     <WelcomeScreen
     // eslint-disable-next-line react/prop-types
-      errorsCount = {props.errorsCount}
+      errorsCount={errorsCount}
     />
   </React.Fragment>;
 };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const ERRORS_COUNT = 5;
 
 ReactDOM.render(
     <App
-      errorsCount = {ERRORS_COUNT}
+      errorsCount={ERRORS_COUNT}
     />,
     document.querySelector(`#root`)
 );
