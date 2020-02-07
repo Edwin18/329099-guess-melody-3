53c77b335b653da16ac56d191ef96e8206503edb
Edwin18 <igreenbergi78@gmail.com>
2020-02-07 15:44:15 +0200

    Устанавливает и применяет PropTypes в проект.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WelcomeScreen from '../WelcomeScreen/WelcomeScreen.jsx';
-// eslint-disable-next-line react/prop-types
+
 const App = ({errorsCount}) => {
   return <React.Fragment>
     <WelcomeScreen
-    // eslint-disable-next-line react/prop-types
       errorsCount={errorsCount}
     />
   </React.Fragment>;
 };
 
 export default App;
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
