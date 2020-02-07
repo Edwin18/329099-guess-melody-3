9691d10eeb28bf752a616884594a8ee251217703
Edwin18 <igreenbergi78@gmail.com>
2020-02-07 17:33:35 +0200

    Добавляет тесты для компонентов

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import WelcomeScreen from '../WelcomeScreen/WelcomeScreen.jsx';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+
+const welcomeButtonHandler = () => {};
 
 const App = ({errorsCount}) => {
   return <React.Fragment>
     <WelcomeScreen
       errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
     />
   </React.Fragment>;
 };
 
-export default App;
-
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
 };
+
+export default App;
